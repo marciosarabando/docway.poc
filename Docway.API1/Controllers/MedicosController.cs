a28b2cd3172dc74eea0f91a8a5e29cf203dc8f3e
Marcio Sarabando <marciosarabando@MacBook-Pro-de-Marcio.local>
2020-04-15 10:43:45 -0300

    Alteraçao da Controller

--- Docway.API1/Controllers/MedicosController.cs
+++ Docway.API1/Controllers/MedicosController.cs
@@ -15,9 +15,37 @@ namespace Docway.API1.Controllers
     {
         [HttpGet]
         [Authorize]
-        public IEnumerable getAll()
+        public IEnumerable<Medicos> getAll()
         {
-            return new string[] { "Dr. Drauzio", "Dr. Dayan Siebra", "Dr. Barakat"};
+            var medico1 = new Medicos();
+            medico1.Id = 1;
+            medico1.Nome = "Dr. Dayan Siebra";
+            medico1.Especialidade = "Ortomolecular";
+
+            var medico2 = new Medicos();
+            medico2.Id = 2;
+            medico2.Nome = "Dr. Barakat";
+            medico2.Especialidade = "Nutrólogo";
+
+            var medico3 = new Medicos();
+            medico3.Id = 3;
+            medico3.Nome = "Dr. Vitor Azzini";
+            medico3.Especialidade = "Nutriendocrinologia";
+
+            var medicos = new List<Medicos>();
+            medicos.Add(medico1);
+            medicos.Add(medico2);
+            medicos.Add(medico3);
+
+            //return new string[] { "Dr. Drauzio", "Dr. Dayan Siebra", "Dr. Barakat"};
+            return medicos;
         }
+    }
+
+    public class Medicos
+    {
+        public int Id  { get; set; }
+        public string Nome { get; set; }
+        public string Especialidade  { get; set; }
     }
 }
